10afbab43e9a4c5696cb5f917df37bab2ea7a138
Finn J. Kane <fjkane@protonmail.com>
2025-12-08 10:56:42 -0700

    added install sageattention script

---
+++ scripts/install-sageattention.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Enable Venv
+source /workspace/runpod-slim/ComfyUI/.venv/bin/activate
+
+pip install triton
+pip install ../sageattention-2.2.0-cu124-cp312-cp312-linux_x86_64.whl
