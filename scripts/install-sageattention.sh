cfbf9b38d68e23b9d31a471de2ede6b0aa1cd2a2
Finn J. Kane <fjkane@protonmail.com>
2025-12-08 11:07:15 -0700

    fixed wheel error

--- scripts/install-sageattention.sh
+++ scripts/install-sageattention.sh
@@ -4,4 +4,4 @@
 source /workspace/runpod-slim/ComfyUI/.venv/bin/activate
 
 pip install triton
-pip install ../wheels/cu124/sageattention-2.2.0-cu124-cp312-cp312-linux_x86_64.whl
+pip install ../wheels/cu124/sageattention-2.2.0-cp312-cp312-linux_x86_64.whl
